cf8e05cb26852376a0d32c1337997293d61890d4
Çağdaş Çalayır <cagdascalayir@outlook.com.tr>
2026-05-18 12:40:53 +0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './Router'
 
 // Bootstrap CSS ve JS import işlemleri
 import 'bootstrap/dist/css/bootstrap.min.css'
